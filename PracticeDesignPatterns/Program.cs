49ac1647a84f45bad585d003684169f904e31848
Ramanagouda Patil <ramanagouda.patil@hidglobal.com>
2025-05-28 16:26:32 +0530

    Added Strategy pattern

---
+++ PracticeDesignPatterns/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PracticeDesignPatterns
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			ILogger logger = LoggerFactory.GetLogger("file");
+			logger.Log("This is a log message.");
+
+		}
+	}
+
+	public interface ILogger
+	{
+		void Log(string message);
+	}
+
+	public class FileLogger : ILogger
+	{
+		public void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+
+	public class DatabaseLogger : ILogger
+	{
+		public void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+
+	public class ConsoleLogger : ILogger
+	{
+		public void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+
+	public static class LoggerFactory
+	{
+		public static ILogger GetLogger(string LogType)
+		{
+			if (LogType == "file") { 
+				return new FileLogger();
+			}
+			if (LogType == "db")
+			{
+				return new FileLogger();
+			}
+			if (LogType == "console")
+			{
+				return new FileLogger();
+			}
+			else
+			{
+				throw new ArgumentException("Invalid Logger Type");
+			}
+		}
+	}
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Strategy
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var paymentContext = new PaymentContext(new CreditCardPayment());
+			paymentContext.ProcessPayment(1000);
+
+			paymentContext.SetStrategy(new PayPalPayment());
+			paymentContext.ProcessPayment(500);
+
+			paymentContext.SetStrategy(new BitcoinPayment());
+			paymentContext.ProcessPayment(750);
+		}
+	}
+
+	public interface IPaymentStrategy
+	{
+		void Pay(decimal amount);
+	}
+
+	public class CreditCardPayment : IPaymentStrategy
+	{
+		public void Pay(decimal amount)
+		{
+			Console.WriteLine($"Paid {amount} using Credit Card.");
+		}
+	}
+
+	public class PayPalPayment : IPaymentStrategy
+	{
+		public void Pay(decimal amount)
+		{
+			Console.WriteLine($"Paid {amount} using PayPal.");
+		}
+	}
+
+	public class BitcoinPayment : IPaymentStrategy
+	{
+		public void Pay(decimal amount)
+		{
+			Console.WriteLine($"Paid {amount} using Bitcoin.");
+		}
+	}
+
+	public class PaymentContext
+	{
+		private IPaymentStrategy _paymentStrategy;
+
+		public PaymentContext(IPaymentStrategy paymentStrategy)
+		{
+			_paymentStrategy = paymentStrategy;
+		}
+
+		public void SetStrategy(IPaymentStrategy strategy)
+		{
+			_paymentStrategy = strategy;
+		}
+
+		public void ProcessPayment(decimal amount)
+		{
+			_paymentStrategy.Pay(amount);
+		}
+	}
+
+}
